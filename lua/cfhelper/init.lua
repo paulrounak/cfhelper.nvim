4056b2c0bc18d202d76aa54e262bd2484adfe3de
duckyfied <rounakpaul007@gmail.com>
2025-05-22 01:21:17 +0530

    Revert "Add CFSetup UI"
    This reverts commit b9ae3fc2a688b94f5beb0fb96e8d631b677fb69e.

--- lua/cfhelper/init.lua
+++ lua/cfhelper/init.lua
@@ -2,11 +2,11 @@ local cf_run = require("cfhelper.CFRun")
 local cf_setup = require("cfhelper.CFSetup")
 
 vim.api.nvim_create_user_command("CFRun", function()
-	cf_run.run()
+  cf_run.run()
 end, {})
 
-vim.api.nvim_create_user_command("CFSetup", function()
-	require("cfhelper.setup_ui").input_url(function(url)
-		cf_setup.setup(url)
-	end)
-end, {})
+vim.api.nvim_create_user_command("CFSetup", function(opts)
+  cf_setup.setup(opts.args)
+end, {
+  nargs = 1,
+})
