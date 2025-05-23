2cd3a4cfbd91037a8e9099c04f1360aa8a664faa
duckyfied <rounakpaul007@gmail.com>
2025-05-23 23:52:03 +0530

    Add support for multiple examples in CodeForces

--- lua/cfhelper/CFRun.lua
+++ lua/cfhelper/CFRun.lua
@@ -13,45 +13,58 @@ local function ensure_trailing_newline(file)
 end
 
 function M.run()
-	local cpp_file = vim.fn.expand("%:p") -- Full path to current file
-	local file_dir = vim.fn.expand("%:p:h") -- Directory of the current file
-
+	local cpp_file = vim.fn.expand("%:p")
+	local file_dir = vim.fn.expand("%:p:h")
 	local helper_dir = file_dir .. "/.cfhelper"
 	vim.fn.mkdir(helper_dir, "p")
 
 	local exec = helper_dir .. "/CFResult.out"
 	local compile_cmd = string.format("g++ -o %s %s", exec, cpp_file)
-	local result = vim.fn.system(compile_cmd)
+	local compile_output = vim.fn.system(compile_cmd)
 
 	if vim.v.shell_error ~= 0 then
 		local error_path = helper_dir .. "/compile_error.txt"
-		local f, err = io.open(error_path, "w")
+		local f = io.open(error_path, "w")
 		if f then
-			f:write(result)
+			f:write(compile_output)
 			f:close()
 			print("Compilation failed. See .cfhelper/compile_error.txt")
 		else
-			print("Compilation failed and couldn't write error file:", err)
+			print("Compilation failed and couldn't write error file.")
 		end
 		return
 	end
 
-	local input_path = helper_dir .. "/input.txt"
-	local output_path = helper_dir .. "/output.txt"
-	local result_path = helper_dir .. "/result.txt"
+	-- Detect number of test cases
+	local i = 1
+	while true do
+		local input_file = string.format("%s/input%d.txt", helper_dir, i)
+		local output_file = string.format("%s/output%d.txt", helper_dir, i)
+		if vim.fn.filereadable(input_file) == 0 or vim.fn.filereadable(output_file) == 0 then
+			break
+		end
 
-	local run_cmd = string.format("%s < %s > %s", exec, input_path, result_path)
-	os.execute(run_cmd)
+		local result_file = string.format("%s/result%d.txt", helper_dir, i)
+		local run_cmd = string.format("%s < %s > %s", exec, input_file, result_file)
+		os.execute(run_cmd)
 
-	ensure_trailing_newline(output_path)
-	ensure_trailing_newline(result_path)
+		ensure_trailing_newline(output_file)
+		ensure_trailing_newline(result_file)
 
-	vim.fn.system(string.format("diff -q %s %s", output_path, result_path))
+		local diff_cmd = string.format("diff -q %s %s", output_file, result_file)
+		vim.fn.system(diff_cmd)
 
-	if vim.v.shell_error == 0 then
-		print("Passed")
-	else
-		print("Failed")
+		if vim.v.shell_error == 0 then
+			print(string.format("Test case #%d: Passed", i))
+		else
+			print(string.format("Test case #%d: Failed", i))
+		end
+
+		i = i + 1
+	end
+
+	if i == 1 then
+		print("No test cases found.")
 	end
 end
 
--- lua/cfhelper/CFSetup.lua
+++ lua/cfhelper/CFSetup.lua
@@ -3,69 +3,92 @@ local ui = require("cfhelper.CFSetup_ui")
 
 -- Extract each input line from <div ...>...</div> inside the first <pre>
 local function extract_input_lines(pre_block)
-  local lines = {}
-  for line in pre_block:gmatch("<div[^>]->(.-)</div>") do
-    line = line:gsub("&lt;", "<"):gsub("&gt;", ">"):gsub("&amp;", "&")
-    table.insert(lines, line)
-  end
-  return table.concat(lines, "\n")
+	local lines = {}
+	for line in pre_block:gmatch("<div[^>]->(.-)</div>") do
+		line = line:gsub("&lt;", "<"):gsub("&gt;", ">"):gsub("&amp;", "&")
+		table.insert(lines, line)
+	end
+	return table.concat(lines, "\n")
 end
 
--- Extract plain text output from the second <pre> block
+-- Extract plain text output from the <pre> block
 local function extract_output(pre_block)
-  return pre_block:gsub("&lt;", "<"):gsub("&gt;", ">"):gsub("&amp;", "&"):gsub("^%s+", ""):gsub("%s+$", "")
+	return pre_block:gsub("&lt;", "<"):gsub("&gt;", ">"):gsub("&amp;", "&"):gsub("^%s+", ""):gsub("%s+$", "")
+end
+
+-- Extract all <pre>...</pre> blocks from the HTML
+local function extract_all_pre_blocks(html)
+	local pre_blocks = {}
+	for block in html:gmatch("<pre>(.-)</pre>") do
+		table.insert(pre_blocks, block)
+	end
+	return pre_blocks
 end
 
 local function fetch_and_parse(url)
-  local file_dir = vim.fn.expand("%:p:h")
-  local helper_dir = file_dir .. "/.cfhelper"
-  vim.fn.mkdir(helper_dir, "p")
+	local file_dir = vim.fn.expand("%:p:h")
+	local helper_dir = file_dir .. "/.cfhelper"
 
-  local html_path = helper_dir .. "/problem.html"
-  local curl_cmd = string.format(
-    "curl -sL --compressed -A 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0 Safari/537.36' -e 'https://codeforces.com' '%s' -o '%s'",
-    url,
-    html_path
-  )
-  os.execute(curl_cmd)
+	-- Clean previous .cfhelper
+	if vim.fn.isdirectory(helper_dir) == 1 then
+		vim.fn.delete(helper_dir, "rf")
+	end
 
-  local f = io.open(html_path, "r")
-  if not f then
-    print("Couldn't read problem.html")
-    return
-  end
+	vim.fn.mkdir(helper_dir, "p")
 
-  local html = f:read("*a")
-  f:close()
+	local html_path = helper_dir .. "/problem.html"
+	local curl_cmd = string.format(
+		"curl -sL --compressed -A 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0 Safari/537.36' -e 'https://codeforces.com' '%s' -o '%s'",
+		url,
+		html_path
+	)
+	os.execute(curl_cmd)
 
-  local input_pre = html:match("<pre>(.-)</pre>")
-  local after_input = html:match("<pre>.-</pre>(.+)")
-  local output_pre = after_input and after_input:match("<pre>(.-)</pre>")
+	local f = io.open(html_path, "r")
+	if not f then
+		print("Couldn't read problem.html")
+		return
+	end
 
-  if not input_pre or not output_pre then
-    print("Failed to parse sample input/output from problem.html")
-    return
-  end
+	local html = f:read("*a")
+	f:close()
 
-  local input_txt = extract_input_lines(input_pre)
-  local output_txt = extract_output(output_pre)
+	local pre_blocks = extract_all_pre_blocks(html)
 
-  local function write(name, content)
-    f = io.open(helper_dir .. "/" .. name, "w")
-    if f then
-      f:write(content)
-      f:close()
-    end
-  end
+	if #pre_blocks < 2 then
+		print("Failed to parse sample input/output from problem.html")
+		return
+	end
 
-  write("input.txt", input_txt)
-  write("output.txt", output_txt)
+	local test_count = math.floor(#pre_blocks / 2)
+	for i = 1, test_count do
+		local input_block = pre_blocks[i * 2 - 1]
+		local output_block = pre_blocks[i * 2]
 
-  print("Sample input/output written to .cfhelper/")
+		local input_text = extract_input_lines(input_block)
+		local output_text = extract_output(output_block)
+
+		local input_path = string.format("%s/input%d.txt", helper_dir, i)
+		local output_path = string.format("%s/output%d.txt", helper_dir, i)
+
+		local in_file = io.open(input_path, "w")
+		if in_file then
+			in_file:write(input_text)
+			in_file:close()
+		end
+
+		local out_file = io.open(output_path, "w")
+		if out_file then
+			out_file:write(output_text)
+			out_file:close()
+		end
+	end
+
+	print(string.format("Wrote %d sample test case(s) to .cfhelper/", test_count))
 end
 
 function M.setup()
-  ui.prompt_url(fetch_and_parse)
+	ui.prompt_url(fetch_and_parse)
 end
 
 return M
